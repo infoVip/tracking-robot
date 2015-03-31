735ca5a78b1f556a0ffab03a5c4a0a9727906522
Joachim Lengacher <j.lengacher@gmx.net>
2015-03-31 21:37:34 +0000

    some simplification and documentation

--- tracking-robot.cpp
+++ tracking-robot.cpp
@@ -25,6 +25,7 @@ using namespace raspicam;
 const string MAIN_WINDOW_NAME = "Robot View";
 const string ROI_WINDOW_NAME = "Region of Interest";
 const int MAX_BINARY_VALUE = 255;
+const int LOOK_AHEAD = 240;
 
 bool headless = false;
 
@@ -44,39 +45,34 @@ int main(int argc, const char *argv[]) {
 		namedWindow(ROI_WINDOW_NAME, WINDOW_AUTOSIZE);
 	}
 
-	cout << "Opening Camera..." << endl;
 	RaspiCam_Cv camera;
-	//camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
 
 	if (camera.open()) {
 		Mat captured;
-		int hight, width;
-		int centerX, centerY;
 		vector < vector <Point> > contours;
 		vector<Vec4i> hierarchy;
 		while (1) {
 			camera.grab();
 			camera.retrieve(captured);
 
-			hight = captured.rows;
-			width = captured.cols;
+			Point capturedImageCenter(captured.cols / 2, captured.rows / 2);
+			circle(captured, capturedImageCenter, 5, Scalar(0, 255, 0), -1, 8, 0);
 
-			centerX = width / 2;
-			centerY = hight / 2;
-
-			//circle(captured, Point(centerX, centerY), 4, Scalar(0, 255, 0));
-			circle(captured, Point(centerX, centerY), 5, Scalar(0, 255, 0), -1, 8, 0);
-
-			Rect roi(0, centerY - 45, width, 100);
+			// region of interest. currently around the center of the
+			// captured image. might need to be moved depending on the
+			// camera angle, speed of the robot etc.
+			Rect roi(0, capturedImageCenter.y - 45, captured.cols, 100);
 			Mat roiImg = captured(roi).clone();
-			cvtColor(roiImg, roiImg, COLOR_BGR2GRAY);
 
+			// extract the line to follow from the background
+			cvtColor(roiImg, roiImg, COLOR_BGR2GRAY);
 			threshold(roiImg, roiImg, threshold_value, MAX_BINARY_VALUE, 1);
 			Mat erodeElmt = getStructuringElement(MORPH_RECT, Size(3, 3));
 			Mat dilateElmt = getStructuringElement(MORPH_RECT, Size(5, 5));
 			erode(roiImg, roiImg, erodeElmt);
 			dilate(roiImg, roiImg, dilateElmt);
 
+			// determine the angle the robot needs to move
 			findContours(roiImg, contours, hierarchy, CV_RETR_TREE,
 					CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 			for (size_t i = 0; i < contours.size(); i++) {
@@ -84,19 +80,17 @@ int main(int argc, const char *argv[]) {
 				if (area > 2000) {
 					Moments mu;
 					mu = moments(contours[i], false);
-					Point2f center(mu.m10 / mu.m00, 240); // point in center (x only)
-					//circle(captured, center, 5, Scalar(0, 255, 0), -1, 8, 0);
-					circle(captured, center, 5, Scalar(0, 255, 0), -1, 8, 0);
-					line(captured, Point(centerX, centerY), center, Scalar(0, 255, 0));
+					Point2f roiCenter(mu.m10 / mu.m00, capturedImageCenter.y - LOOK_AHEAD); // point in center (x only)
+					circle(captured, roiCenter, 5, Scalar(0, 255, 0), -1, 8, 0);
+					line(captured, capturedImageCenter, roiCenter, Scalar(0, 255, 0));
 
-					cout << "ak=" << centerY - 240 << ", gk=" << centerX - mu.m10 / mu.m00 << endl;
-					float ankathete = centerY - 240;
-					float gegenkathete = centerX - center.x;
+					float ankathete = LOOK_AHEAD;
+					float gegenkathete = capturedImageCenter.x - roiCenter.x;
 					float bogenmass = atan(gegenkathete/ankathete);
 					float gradmass = bogenmass*180/3.1415926535;
-					cout << "winkel=" << gradmass << " Grad" << endl;
-					//putText(captured, text, textOrg, fontFace, fontScale,
-					//        Scalar::all(255), thickness, 8);
+
+					string caption = format("%f Grad", gradmass);
+					putText(captured, caption, Point(capturedImageCenter.x + 20, capturedImageCenter.y + 20),  FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0));
 				}
 			}
 
